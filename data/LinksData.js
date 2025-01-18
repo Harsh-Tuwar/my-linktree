bcc9c4bce1566b675e53c9853a33d9d8ab501c3e
Harsh Tuwar <tuwarharsh08@gmail.com>
2025-01-18 23:59:34 +0530

    updated video

--- data/LinksData.js
+++ data/LinksData.js
@@ -116,7 +116,8 @@ const webLinks = [
     // },
     {
         title: 'Wedding Invitation',
-        url: 'https://www.canva.com/design/DAGboPY8FJQ/zBrX4rNs7BKY47UaNzXU5w/watch?utm_content=DAGboPY8FJQ&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h18764931dd',
+        // url: 'https://www.canva.com/design/DAGboPY8FJQ/zBrX4rNs7BKY47UaNzXU5w/watch?utm_content=DAGboPY8FJQ&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h18764931dd',
+        url: 'https://drive.google.com/file/d/1gMWhVTDOpVgipXHKZOdvFG2WGkMXKI14/view?usp=sharing',
         type: 'install',
         icon: '/assets/heart.png',
         on: true
